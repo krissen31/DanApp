c7ec3d3775efd9a3dba6037b8956744a507098fb
krissen31 <krissen31@gmail.com>
2017-07-25 22:24:29 +0100

    Added controller and set up reg form to the page. Works only if every field has any value. Require entry validation.

---
+++ DanApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DanApp.Models;
+
+namespace DanApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Registration obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                DanAppDBEntities db = new DanAppDBEntities();
+                db.Registrations.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+    }
+}
